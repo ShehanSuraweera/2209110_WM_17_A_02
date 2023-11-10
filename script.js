341a99a5e34627560fa50f290735c8ba2fd7fbed
ShehanSuraweera <shehanssuraweera@gmail.com>
2023-11-10 23:36:35 +0530

    imgaes and game updated

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ let images = [
 let dice = document.querySelectorAll("img");
 
 function roll() {
+  function setVisibility(isvisible) {
+    document.getElementById("cup").style.visibility = isvisible;
+    document.getElementById("player-2").style.visibility = isvisible;
+    document.getElementById("player-1").style.visibility = isvisible;
+    document.getElementById("congtraz").style.visibility = isvisible;
+    document.getElementById("winner").style.visibility = isvisible;
+  }
+
+  setVisibility("hidden");
+
   dice.forEach(function (die) {
     die.classList.add("shake");
   });
@@ -23,7 +33,20 @@ function roll() {
     document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
     document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
 
-    document.querySelector("#total").innerHTML =
-      "Your rolll is " + (dieOneValue + 1 + (dieTwoValue + 1));
+    document.querySelector("#player-1").innerHTML =
+      "Your rolll is " + (dieOneValue + 1);
+    document.querySelector("#player-2").innerHTML =
+      "Your rolll is " + (dieTwoValue + 1);
+    document.getElementById("congtraz").style.visibility = "visible";
+    setVisibility("visible");
+    if (dieOneValue == dieTwoValue) {
+      document.getElementById("congtraz").style.visibility = "hidden";
+      document.getElementById("cup").style.visibility = "hidden";
+      document.querySelector("#winner").innerHTML = "Game Draw! Role again...";
+    } else if (dieOneValue < dieTwoValue) {
+      document.querySelector("#winner").innerHTML = "Palyer 02 WON!";
+    } else {
+      document.querySelector("#winner").innerHTML = "Palyer 01 WON!";
+    }
   }, 1500);
 }
